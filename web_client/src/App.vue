680a5aeff3166bf3d9a0b7ea0740cf7e1e968ec8
Jaeyong Choi <jayong93@gmail.com>
2021-06-15 00:57:33 +0900

    check accessing to lambda function

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -1,19 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <amplify-auth-container>
+      <amplify-authenticator>
+        <div v-if="authState === 'signedin' && user">{{ user.username }}</div>
+        <button v-if="authState === 'signedin' && user" v-on:click="load_data">
+          load data
+        </button>
+        <amplify-sign-out></amplify-sign-out>
+      </amplify-authenticator>
+    </amplify-auth-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "regenerator-runtime/runtime";
+import "@aws-amplify/ui-vue";
+import { onAuthUIStateChange } from "@aws-amplify/ui-components";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+      console.log(authData);
+    });
+  },
+  data() {
+    return {
+      authState: undefined,
+      user: undefined,
+      unsubscribeAuth: undefined,
+    };
+  },
+  methods: {
+    load_data() {
+      axios
+        .get(
+          "https://51c22t1aba.execute-api.ap-northeast-2.amazonaws.com/test",
+          {
+            headers: {
+              Authorization:
+                "Bearer " + this.user.signInUserSession.idToken.jwtToken,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+  beforeDestroy() {
+    this.unsubscribeAuth();
+  },
+};
 </script>
 
 <style>
--- web_client/src/main.js
+++ web_client/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import awsconfig from "./aws-exports";
+import Amplify from "aws-amplify";
+Amplify.configure(awsconfig);
 
 Vue.config.productionTip = false
 
